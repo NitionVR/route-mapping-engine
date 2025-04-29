a785985f776b30b76a6e07f6c5f142d0456f46a2
NitionVR <kmaganyane83@gmail.com>
2025-04-29 14:34:50 +0200

    Implement and test requestPermission method

--- src/core/Services/LocationService.ts
+++ src/core/Services/LocationService.ts
@@ -1,3 +1,4 @@
+import {PermissionStatus} from "../models/PermissionStatus";
 
 export class LocationService{
 
@@ -6,5 +7,29 @@ export class LocationService{
         return 'geolocation' in navigator &&
             typeof navigator.geolocation !== 'undefined' &&
             typeof navigator.geolocation.getCurrentPosition === 'function';
+    }
+
+    async requestPermission() : Promise<PermissionStatus> {
+        if (!this.isAvailable()) {
+            throw new Error('Geolocation API is not available');
+        }
+
+        return new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    console.log('Initial position is: ', {
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude,
+                        accuracy: position.coords.accuracy
+                    });
+                    resolve(PermissionStatus.GRANTED);
+                },
+                (error) => {
+                    switch(error.code){
+
+                    }
+                }
+            )
+        });
     }
 }
--- tests/core/model/LocationService.test.ts
+++ tests/core/model/LocationService.test.ts
@@ -1,4 +1,5 @@
 import {LocationService} from "../../../src/core/Services/LocationService";
+import {PermissionStatus} from "../../../src/core/models/PermissionStatus";
 
 const mockGeoLocation = () =>{
     const getCurrentPositionMock = jest.fn();
@@ -21,29 +22,66 @@ const mockGeoLocation = () =>{
     };
 };
 
+const mockPermissions = (state = 'granted') =>{
+    const permissionMock = jest.fn().mockResolvedValue({state});
+
+    Object.defineProperty(global.navigator, 'permissions', {
+        value: {
+            permission: permissionMock
+        },
+        configurable: true
+    });
+
+    return permissionMock;
+};
+
 describe('LocationService', ()=>{
 
-    // As a user, I want to determine if GeoLocation API is available in the browser
-    // So that I can proceed with location-based functionality
     let locationService: LocationService;
+    let mockGeo: ReturnType<typeof mockGeoLocation>
 
     beforeEach(()=>{
         locationService = new LocationService();
+        mockGeo = mockGeoLocation()
     });
 
-    afterEach(() => {
-        Object.defineProperty(global.navigator, 'geolocation',{
-            value: undefined,
-            configurable: true,
-        })
-    });
+    // As a user, I want to determine if GeoLocation API is available in the browser
+    // So that I can proceed with location-based functionality
 
     test('isAvailable should return true when geolocation is available', ()=>{
-        let mockGeo: ReturnType<typeof mockGeoLocation> = mockGeoLocation();
         expect(locationService.isAvailable()).toBe(true);
     });
 
     test('isAvailable should return false when geolocation is not available', ()=>{
+        Object.defineProperty(global.navigator, 'geolocation',{
+            value: undefined,
+            configurable: true,
+        })
         expect(locationService.isAvailable()).toBe(false);
     });
+
+    // As a user, I want to request permission to access my location so that I can use
+    // location-based functionality
+
+    test("requestPermission should return GRANTED when permission  is granted", async () => {
+        let mockedPermission = mockPermissions('granted');
+
+        mockGeo.getCurrentPositionMock.mockImplementation((success)=>{
+           success({
+               coords: {
+                   latitude: 0,
+                   longitude: 0,
+                   accuracy: 0,
+                   altitude: null,
+                   altitudeAccuracy: null,
+                   heading: null,
+                   speed: null,
+               },
+               timestamp: 0,
+           });
+        });
+        const result = await locationService.requestPermission();
+        expect(result).toBe(PermissionStatus.GRANTED);
+    });
+
 });
